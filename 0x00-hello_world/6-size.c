2eece55735ccc6bf7331ef55f291555337cf3531
mohamed hesham <shinetym@gmail.com>
2023-07-13 19:04:56 -0700

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: A C program print size of variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long: %lu byte(s)", sizeof(long));
+	printf("Size of a long long: %lu byte(s)", sizeof(long long));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+
+	return (0);
+}
