7d96e280025e8f2c125b214089631a8d0eed2958
mohamed hesham <shinetym@gmail.com>
2023-07-13 18:43:49 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Discription: A C program that print with Puts function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
