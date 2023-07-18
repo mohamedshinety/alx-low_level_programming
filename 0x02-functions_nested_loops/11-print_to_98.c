316daca51a1e3ef9b97d1f391d9ace9cbf22faa8
mohamed hesham <shinetym@gmail.com>
2023-07-18 14:40:13 -0700

    a

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - utilized on the _putchar function to print
+ * The alphabet a - z
+ * @n: paramter
+*/
+
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; count--)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; count++)
+			printf("%d, ", count);
+	printf("98\n");
+}
