fc53806138c35cf5cdf4042ed90963eeefab6e51
mohamed hesham <shinetym@gmail.com>
2023-07-18 14:18:32 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,23 +11,24 @@ void times_table(void)
 
 	for (line = 0; line <= 9; line++)
 	{
+		_putchar(48);
 		for (mult = 0; mult <= 9; mult++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			res = mult * line;
+
 			if (res <= 9)
 			{
-				_putchar(res + 48);
-				_putchar(',');
-                        	_putchar(' ');
 				_putchar(' ');
 			}
 			else
-			{
+			
 				_putchar((res / 10) + 48);
-				_putchar((res % 10) + 48);
-				_putchar(',');
-				_putchar(' ');
-			}
+
+			_putchar((res % 10) + 48);
+			
 		}
 		_putchar('\n');
 	}
