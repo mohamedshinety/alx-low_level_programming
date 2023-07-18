1bce00837c954cc5da3a501ad2fb6b73621f04d5
mohamed hesham <shinetym@gmail.com>
2023-07-18 13:42:38 -0700

    a

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - utilized on the _putchar function to print
+ * The alphabet a - z
+*/
+
+void times_table(void)
+{
+	int mult, line;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (mult = 0; mult <= 9; mult++)
+			mult = mult * line;
+		if (mult <= 9)
+			_putchar(mult + 48);
+		_putchar((mult / 10) + 48);
+		_putchar((mult % 10) + 48);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
