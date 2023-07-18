048a0b74966b8b0d923b9c1c77a33f14e37799e9
mohamed hesham <shinetym@gmail.com>
2023-07-18 14:28:07 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - utilized on the _putchar function to print
+ * The alphabet a - z
+ *
+ * @a: check input of function
+ * @b: check input of function
+ *
+ * Return: n if c is lowercase
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
