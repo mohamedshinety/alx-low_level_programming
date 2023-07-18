3af220ffb7cf4aa6a91800a517cef030781dbb2a
mohamed hesham <shinetym@gmail.com>
2023-07-18 12:26:31 -0700

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - utilized on the _putchar function to print
+ * The alphabet a - z
+ *
+ * @n: check input of function
+ *
+ * Return: 1 if c is lowercase
+*/
+
+int _abs(int n);
+{
+	if (n < 0)
+		n = n * (-1);
+	return (n);
+}
