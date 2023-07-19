38969d1a4da206e5cbbd33afad0f2d5438cfd8e6
mohamed hesham <shinetym@gmail.com>
2023-07-18 18:09:16 -0700

    a

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - utilized on the _putchar function to print
+ * The alphabet a - z
+ * @n: paramter
+*/
+
+void print_times_table(int n)
+{
+
+	int mult, line, res;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (line = 0; line <= n; line++)
+		{
+			_putchar(48);
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				res = mult * line;
+
+				if (res <= 9)
+				{
+					_putchar(' ');
+				}
+				else if (res < 100)
+				{
+					_putchar((res / 10) + 48);
+					_putchar((res % 10) + 48);
+				}
+				else if (res > 100)
+				{
+					_putchar((res / 100) + 48);
+					_putchar((res % 100) + 48);
+				}
+				else
+					_putchar((res / 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -27,6 +27,8 @@ int add(int, int);
 
 void print_to_98(int n);
 
+void print_times_table(int n);
+
 
 
 #endif
